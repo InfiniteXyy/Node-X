ed23612df0d1a4b90e5f1b750986f2de8cdbcfb6
xyy <727160222@qq.com>
2017-12-06 21:12:11 +0800

    haha

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -1,6 +1,6 @@
 package com.homework;
 public class Main {
-    int a = 2;
+    int b = 2;
     public static void main(String[] args) {
         com.homework.lym.Main.main(args);
 	// write your code here
